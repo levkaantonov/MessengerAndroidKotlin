1b4906d96925c4d8118cb2d51ad7af6073de2d21
levkaantonov <levkaantonov@gmail.com>
2021-02-26 23:58:24 +0300

    added drawer and fragments

--- app/src/main/java/levkaantonov/com/study/telegaclone/MainActivity.kt
+++ app/src/main/java/levkaantonov/com/study/telegaclone/MainActivity.kt
@@ -1,11 +1,42 @@
 package levkaantonov.com.study.telegaclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import levkaantonov.com.study.telegaclone.databinding.ActivityMainBinding
+import levkaantonov.com.study.telegaclone.ui.ChatsFragment
+import levkaantonov.com.study.telegaclone.ui.objects.AppDrawer
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val mBinding get() = _binding!!
+    private lateinit var mAppDrawer: AppDrawer
+    private lateinit var mToolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunctionality()
+    }
+
+    private fun initFunctionality() {
+        setSupportActionBar(mToolbar)
+        mAppDrawer.create()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.dataContainer, ChatsFragment())
+            .commit()
+    }
+
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
+        mAppDrawer = AppDrawer(this, mToolbar)
     }
 }
